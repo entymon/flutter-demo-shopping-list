04198e40af2596cbf41e6afbfde701093766d596
entymon <pawel.olejniczak.i@gmail.com>
2024-01-27 09:45:33 +0100

    Handle errors from backend

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -16,6 +16,7 @@ class GroceryList extends StatefulWidget {
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
   var _isLoading = true;
+  String? _error;
 
   @override
   void initState() {
@@ -25,10 +26,17 @@ class _GroceryListState extends State<GroceryList> {
 
   void _loadItems() async {
     final url = Uri.https(
-      'flutter-demo-backend-2aa4b-default-rtdb.europe-west1.firebasedatabase.app',
+      // 'flutter-demo-backend-2aa4b-default-rtdb.europe-west1.firebasedatabase.app',
+      'abc.europe-west1.firebasedatabase.app',
       'shopping-list.json',
     );
     final response = await http.get(url);
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _error = 'Failed to fetch data. Please try again later.';
+      });
+    }
     final Map<String, dynamic> listData = json.decode(response.body);
     final List<GroceryItem> loadedItems = [];
     for (final item in listData.entries) {
@@ -104,6 +112,12 @@ class _GroceryListState extends State<GroceryList> {
             ),
           ),
         ),
+      );
+    }
+
+    if (_error != null) {
+      content = Center(
+        child: Text(_error!),
       );
     }
 
