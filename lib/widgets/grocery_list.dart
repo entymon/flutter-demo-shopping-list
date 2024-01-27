5d20984ce0b3934c18d85da7b235980fe849ab8b
entymon <pawel.olejniczak.i@gmail.com>
2024-01-27 10:08:27 +0100

    Add try catch error handling

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -29,40 +29,47 @@ class _GroceryListState extends State<GroceryList> {
       'flutter-demo-backend-2aa4b-default-rtdb.europe-west1.firebasedatabase.app',
       'shopping-list.json',
     );
-    final response = await http.get(url);
+    try {
+      final response = await http.get(url);
 
-    if (response.statusCode >= 400) {
+      if (response.statusCode >= 400) {
+        setState(() {
+          _error = 'Failed to fetch data. Please try again later.';
+        });
+      }
+
+      if (response.body == 'null') {
+        setState(() {
+          _isLoading = false;
+        });
+      } else {
+        final Map<String, dynamic> listData = json.decode(response.body);
+        final List<GroceryItem> loadedItems = [];
+        for (final item in listData.entries) {
+          final category = categories.entries
+              .firstWhere(
+                  (catItem) => catItem.value.title == item.value['category'])
+              .value;
+          loadedItems.add(
+            GroceryItem(
+                id: item.key,
+                name: item.value['name'],
+                quantity: item.value['quantity'],
+                category: category),
+          );
+        }
+
+        setState(() {
+          _groceryItems = loadedItems;
+          _isLoading = false;
+        });
+      }
+    } catch (error) {
+      print(error);
       setState(() {
-        _error = 'Failed to fetch data. Please try again later.';
+        _error = 'Something went wrong. Please try again later.';
       });
     }
-
-    if (response.body == 'null') {
-      setState(() {
-        _isLoading = false;
-      });
-    }
-
-    final Map<String, dynamic> listData = json.decode(response.body);
-    final List<GroceryItem> loadedItems = [];
-    for (final item in listData.entries) {
-      final category = categories.entries
-          .firstWhere(
-              (catItem) => catItem.value.title == item.value['category'])
-          .value;
-      loadedItems.add(
-        GroceryItem(
-            id: item.key,
-            name: item.value['name'],
-            quantity: item.value['quantity'],
-            category: category),
-      );
-    }
-
-    setState(() {
-      _groceryItems = loadedItems;
-      _isLoading = false;
-    });
   }
 
   void _addItem() async {
